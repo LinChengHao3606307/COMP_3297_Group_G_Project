c5aa9dd147eb96784b11f2a9c55d043d25be5fbf
vinkami <vinkami@vinkami.com>
2026-03-28 05:13:24 +0800

    Registered all models in admin page

--- src/api/admin.py
+++ src/api/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import *
+
+
+admin.site.register(Product)
+admin.site.register(Report)
+admin.site.register(User)
+admin.site.register(Comment)
