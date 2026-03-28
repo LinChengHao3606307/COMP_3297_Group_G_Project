3bf615dddb4acadb582dc4e9d72f1a9d85b124a6
vinkami <vinkami@vinkami.com>
2026-03-28 23:47:45 +0800

    Created __str__ methods for better admin page handling. Changed: _id -> id

--- src/api/models.py
+++ src/api/models.py
@@ -2,22 +2,28 @@ from django.db import models
 
 
 class User(models.Model):
-    _id = models.AutoField(primary_key=True)
-    person_id = models.IntegerField()
+    id = models.AutoField(primary_key=True)
+    user_id = models.IntegerField()
     # Tester, Developer, Project Owner
     role = models.CharField(max_length=20, choices=(("T", "Tester"), ("D", "Developer"), ("PO", "Project Owner")))
 
+    def __str__(self):
+        return f"{self.role} #{self.user_id}"
+
 
 class Product(models.Model):
-    title = models.CharField(max_length=100)
-    _id = models.AutoField(primary_key=True)
+    id = models.AutoField(primary_key=True)
     owner = models.ForeignKey(User, on_delete=models.CASCADE)
 
+    name = models.CharField(max_length=100)
     version = models.CharField(max_length=20)
+
+    def __str__(self):
+        return f"Product #{self.id} {self.name} v{self.version} ({self.owner})"
 
 
 class Report(models.Model):
-    _id = models.AutoField(primary_key=True)
+    id = models.AutoField(primary_key=True)
     product = models.ForeignKey(Product, on_delete=models.CASCADE)
     owner = models.ForeignKey(User, on_delete=models.CASCADE)
 
@@ -26,10 +32,10 @@ class Report(models.Model):
         ("New", "New"), ("Open", "Open"), ("Assigned", "Assigned"), ("Fixed", "Fixed"), ("Resolved", "Resolved"),
         ("Reopened", "Reopened"), ("Rejected", "Rejected"), ("Duplicate", "Duplicate"), ("Cannot reproduce", "Cannot reproduce"),
     ))
-    priority = models.TextField(choices=(
+    priority = models.TextField(blank=True, choices=(
         ("Critical", "Critical"), ("High", "High"), ("Medium", "Medium"), ("Low", "Low")
     ))
-    severity = models.TextField(choices=(
+    severity = models.TextField(blank=True, choices=(
         ("Critical", "Critical"), ("Major", "Major"), ("Minor", "Minor"), ("Low", "Low")
     ))
     title = models.TextField()
@@ -39,12 +45,18 @@ class Report(models.Model):
 
     timestamp = models.DateTimeField(auto_now_add=True)
 
+    def __str__(self):
+        return f"Report #{self.id} ({self.product}, {self.owner})"
+
 
 class Comment(models.Model):
-    _id = models.AutoField(primary_key=True)
+    id = models.AutoField(primary_key=True)
     report = models.ForeignKey(Report, on_delete=models.CASCADE)
     owner = models.ForeignKey(User, on_delete=models.CASCADE)
 
     text = models.TextField()
 
     timestamp = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"Comment #{self.id} ({self.report}, {self.owner})"
