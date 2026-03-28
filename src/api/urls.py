ffb9706b8b8c5de9bb669897f5e128cb77a801a4
vinkami <vinkami@vinkami.com>
2026-03-28 23:48:42 +0800

    Removed unnecessary code

--- src/api/urls.py
+++ src/api/urls.py
@@ -1,11 +1,9 @@
 from django.urls import path, include
-from .serializers import router
-from .views import *
+from .viewsets import router
 
 app_name = 'api'
 
 urlpatterns = [
-    path("product", product),
     path('', include(router.urls)),
     path('api-auth/', include('rest_framework.urls'), name='api-root'),
 ]
--- src/api/views.py
+++ src/api/views.py
@@ -2,8 +2,3 @@ from rest_framework.decorators import api_view
 from rest_framework.response import Response
 
 from .models import *
-
-@api_view(['GET'])
-def product(request):
-    resp = {"products": Product.objects.all()}
-    return Response(resp)
